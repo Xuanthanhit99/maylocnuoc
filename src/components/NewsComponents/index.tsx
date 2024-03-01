2d609af78742273c039547508253c464eee8ec0f
thanhdx <thanhdx@myvbi.vn>
2024-03-01 13:52:30 +0700

    3/1 1:52

--- src/components/NewsComponents/index.tsx
+++ src/components/NewsComponents/index.tsx
@@ -9,32 +9,32 @@ import Link from "next/link";
 const itemsmenu = [
   {
     id: 1,
-    key: "",
+    key: "/news",
     label: "Tin tức mới",
   },
   {
     id: 2,
-    key: "sua-may-loc-nuoc",
+    key: "/news/sua-may-loc-nuoc",
     label: "Sửa máy lọc nước",
   },
   {
     id: 3,
-    key: "thay-loi-loc-nuoc",
+    key: "/news/thay-loi-loc-nuoc",
     label: "Thay lõi lọc nước",
   },
   {
     id: 4,
-    key: "tu-van",
+    key: "/news/tu-van",
     label: "Tư vấn",
   },
   {
     id: 5,
-    key: "trai-nghiem-khach-hang",
+    key: "/news/trai-nghiem-khach-hang",
     label: "Trải nghiệm khách hàng",
   },
   {
     id: 6,
-    key: "uu-dai-va-khuyen-mai",
+    key: "/news/uu-dai-va-khuyen-mai",
     label: "Ưu đãi & khuyến mãi",
   },
 ];
@@ -46,6 +46,7 @@ const listnews = [
     image: "/image/product/may-loc-nuoc-kangaroo.png",
     time: "16:52 01/02/2024",
     url: "/news/may-loc-nuoc-karofi",
+    link: "/product/may-loc-nuoc-nong-lanh-karofi-kad-d66",
   },
   {
     key: 2,
@@ -53,6 +54,7 @@ const listnews = [
     image: "/image/product/may-loc-nuoc-kangaroo.png",
     time: "16:52 01/02/2024",
     url: "/news/may-loc-nuoc-karofi",
+    link: "/product/may-loc-nuoc-nong-lanh-karofi-kad-d66",
   },
   {
     key: 3,
@@ -60,6 +62,7 @@ const listnews = [
     image: "/image/product/may-loc-nuoc-kangaroo.png",
     time: "16:52 01/02/2024",
     url: "/news/may-loc-nuoc-karofi",
+    link: "/product/may-loc-nuoc-nong-lanh-karofi-kad-d66",
   },
   {
     key: 4,
@@ -67,6 +70,7 @@ const listnews = [
     image: "/image/product/may-loc-nuoc-kangaroo.png",
     time: "16:52 01/02/2024",
     url: "/news/may-loc-nuoc-karofi",
+    link: "/product/may-loc-nuoc-nong-lanh-karofi-kad-d66",
   },
   {
     key: 5,
@@ -74,6 +78,7 @@ const listnews = [
     image: "/image/product/may-loc-nuoc-kangaroo.png",
     time: "16:52 01/02/2024",
     url: "/news/may-loc-nuoc-karofi",
+    link: "/product/may-loc-nuoc-nong-lanh-karofi-kad-d66",
   },
   {
     key: 6,
@@ -81,6 +86,7 @@ const listnews = [
     image: "/image/product/may-loc-nuoc-kangaroo.png",
     time: "16:52 01/02/2024",
     url: "/news/may-loc-nuoc-karofi",
+    link: "/product/may-loc-nuoc-nong-lanh-karofi-kad-d66",
   },
 ];
 
@@ -88,35 +94,34 @@ const News = (props: any) => {
   return (
     <div className="w-full h-auto">
       <div>
-        <div className="bg-[url('/image/tintuc/bg-tintuc.jpg')] w-full h-96 flex justify-center items-center font-medium text-4xl text-center">
+        <div className="bg-[url('/image/tintuc/bg-tintuc.jpg')] sm:justify-around w-full sm:h-56 lg:h-96 xl:h-96 flex justify-center items-center font-medium sm:text-lg lg:text-2xl xl:text-4xl text-center">
           Tin tức mới nhất từ cửa hàng và những chia sẻ
           <br />
           giúp bạn sống vui, sống khỏe mỗi ngày
         </div>
         <div className="flex justify-center h-14 bg-gradient-to-r from-indigo-500 via-sky-500 via-30% to-emerald-500">
-          <ul className="flex py-4 w-9/12">
+          <ul className="py-4 sm:py-0 w-9/12 sm:flex sm:justify-start sm:items-center sm:overflow-auto">
             {itemsmenu?.map((item) => {
               return (
                 <li
                   key={item?.id}
-                  className="cursor-pointer mr-6 hover:text-gray-300  text-white"
+                  className="lg:text-lg xl:text-xl sm:text-sm md:text-md cursor-pointer mr-6 hover:text-gray-300  text-white sm:w-24 sm:flex"
                 >
-                  {item?.label}
+                  <Link href={item?.key} className="sm:flex sm:w-24 sm:h-14 sm:justify-center sm:items-center sm:text-center">{item?.label}</Link>
                 </li>
               );
             })}
           </ul>
+          <div className="flex justify-center items-center text-white">next</div>
         </div>
         <div className="flex justify-center w-full">
           <div className="flex w-9/12 flex-col my-6">
             <div>
-              <div className="w-full grid grid-cols-5 gap-5">
+              <div className="w-full grid grid-cols-1 gap-1 lg:grid-cols-4 lg:gap-4 sm:grid-cols-1 sm:gap-1 md:grid-cols-2 md:gap-2 xl:grid-cols-6 xl:gap-6">
                 {listnews.map((item: any) => {
                   return (
                     <div
-                      className={`${
-                        item.key > 1 && item.key < 6 ? "mr-1" : ""
-                      }`}
+                      className="mr-1 sm:mb-6"
                       key={item.key}
                     >
                       <CardItem
