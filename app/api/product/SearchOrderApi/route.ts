b9f17a46af3f4f73a1161eb44a2f9ba3f217114d
thanhdx <thanhdx@myvbi.vn>
2024-04-03 11:40:41 +0700

    [update][commit]

--- app/api/product/SearchOrderApi/route.ts
+++ app/api/product/SearchOrderApi/route.ts
@@ -9,14 +9,14 @@ export const POST = async (request: NextRequest) => {
         const reqBody = await request.json()
         const {madonhang, phone} = reqBody
         const findAPi = await Cart.findOne({madonhang,phone});
-        console.log("findAPi", findAPi);
+        console.log("findAPi", findAPi.product);
         const ObjectStatus = {
             "trangthai": findAPi?.trangthai,
             "paymentstatus": findAPi?.paymentstatus,
             "orderstatus": findAPi?.orderstatus,
-            "codeorders": findAPi?.codeorders
+            "codeorders": findAPi?.codeorders,
         }
-        return NextResponse.json({data: [findAPi?.informationuser,findAPi?.deliveryaddress,ObjectStatus], success: true})
+        return NextResponse.json({data: [findAPi?.informationuser,findAPi?.deliveryaddress,findAPi.product,ObjectStatus], success: true})
         } catch (error) {
             return NextResponse.json({data:error, success: false})
 
